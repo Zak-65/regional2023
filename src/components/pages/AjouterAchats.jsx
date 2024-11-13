9a2f777080b651eb6d5bbeda704ce5e5dd5bd534
zak-65 <sidqui.zakaria1@gmail.com>
2024-11-13 16:24:11 +0100

    chnage on use selector to ajoutes achats and add filter in tailwaind

--- src/components/pages/AjouterAchats.jsx
+++ src/components/pages/AjouterAchats.jsx
@@ -1,10 +1,17 @@
+import FilterAchats from "../smallComponents/FilterAchats";
 import FormAddAchats from "../smallComponents/FormAddAchat";
-import Table from '../smallComponents/Table'
+import Table from '../smallComponents/Table';
+import { useSelector } from "react-redux";
+
 export default function AjouterAchats() {
+  const achats = useSelector((state) => state.achatReducer.achats);
+  const clients = useSelector((state) => state.clientReducer.clients);
+  const produits = useSelector((state) => state.produitReducer.produits);
   return (
     <>
-      <FormAddAchats />
-      <Table />
+      <FormAddAchats produits={produits} />
+      <FilterAchats/>
+      <Table achats={achats} produits={produits} clients={clients} />
     </>
   );
 }
