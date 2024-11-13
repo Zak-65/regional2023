6fac3037dc272ada3223a87055f3a9926e2b61a1
khalidmarzou <khalidmarzoug@gmail.com>
2024-11-13 21:11:16 +0100

    complete add and delete

--- src/components/smallComponents/Table.jsx
+++ src/components/smallComponents/Table.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { supprimerAchatAction } from "../../redux/actions/achatActions";
 
 export default function Table({ produits, filtredAchats, clients }) {
+  const dispatch = useDispatch();
+  function handleDelete(numero, codeProduit) {
+    dispatch(supprimerAchatAction(numero, codeProduit));
+  }
+
   return (
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <caption className="p-5 text-lg font-semibold text-gray-900 bg-white dark:text-white dark:bg-gray-800">
         All Achats
-        <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Vous pouvez supprimer les achats que vous souhaitez parmi tous les achats effectués.</p>
+        <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+          Vous pouvez supprimer les achats que vous souhaitez parmi tous les achats effectués.
+        </p>
       </caption>
       <thead className="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
@@ -44,13 +52,12 @@ export default function Table({ produits, filtredAchats, clients }) {
               <td className="px-6 py-4">{achat.qte}</td>
               <td className="px-6 py-4">{produit.prix}</td>
               <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
+                <button
                   className="font-medium text-red-600 dark:text-red-500 hover:underline"
-                  onChange={() => handleDelete(achat.numero, achat.codeProduit)}
+                  onClick={() => handleDelete(achat.numero, achat.codeProduit)}
                 >
                   Supprimer
-                </a>
+                </button>
               </td>
             </tr>
           );
