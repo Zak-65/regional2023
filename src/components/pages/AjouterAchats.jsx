95147c2fe85af774e4fb31ecb7dde3d00b52e327
zak-65 <sidqui.zakaria1@gmail.com>
2024-11-13 17:08:57 +0100

    filter in table

--- src/components/pages/AjouterAchats.jsx
+++ src/components/pages/AjouterAchats.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import FilterAchats from "../smallComponents/FilterAchats";
 import FormAddAchats from "../smallComponents/FormAddAchat";
-import Table from '../smallComponents/Table';
+import Table from "../smallComponents/Table";
 import { useSelector } from "react-redux";
 
 export default function AjouterAchats() {
   const achats = useSelector((state) => state.achatReducer.achats);
   const clients = useSelector((state) => state.clientReducer.clients);
   const produits = useSelector((state) => state.produitReducer.produits);
+
+  const [filtredAchats, setFiltredAchats] = useState([...achats]);
+
+  function handleChangeFilter(e) {
+    const clientID = e.target.value;
+    if (!clientID) {
+      setFiltredAchats(achats);
+    } else {
+      const newFiltredAchats = achats.filter((item) => item.numero == clientID);
+      setFiltredAchats(newFiltredAchats);
+    }
+  }
+
   return (
     <>
       <FormAddAchats produits={produits} />
-      <FilterAchats/>
-      <Table achats={achats} produits={produits} clients={clients} />
+      <FilterAchats handleChangeFilter={handleChangeFilter} />
+      <Table filtredAchats={filtredAchats} produits={produits} clients={clients} />
     </>
   );
 }
--- src/components/smallComponents/FilterAchats.jsx
+++ src/components/smallComponents/FilterAchats.jsx
@@ -1,8 +1,11 @@
-export default function FilterAchats(){
+import { useState } from "react"
+
+export default function FilterAchats({handleChangeFilter}){
+
     return(
         <div className="w-full flex justify-center">
 
-            <input type="text" className ="h-10 w-2/4 pr-8 m-4 pl-5 rounded z-0 focus:shadow focus:outline-none " placeholder="Filter Achat ..."/>
+            <input onChange={handleChangeFilter}  type="text" className ="h-10 w-2/4 pr-8 m-4 pl-5 rounded z-0 focus:shadow focus:outline-none " placeholder="Filter Achat ..."/>
 
         </div>
     )
