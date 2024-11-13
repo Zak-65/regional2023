c05b24596bc73268e56d558decc1e228fe9be5e3
zak-65 <sidqui.zakaria1@gmail.com>
2024-11-13 15:44:26 +0100

    final style for table & form

--- src/components/smallComponents/Table.jsx
+++ src/components/smallComponents/Table.jsx
@@ -1,6 +1,5 @@
 export default function Table() {
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <caption className="p-5 text-lg font-semibold text-gray-900 bg-white dark:text-white dark:bg-gray-800">
           All Achats
@@ -40,7 +39,6 @@ export default function Table() {
             </td>
           </tr>
         </tbody>
-      </table>
-    </div>
-  );
+      </table>  
+      );
 }
